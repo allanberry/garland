803752ecd645082befd9b8185f1f8d6830e96a54
Allan Berry <allan.berry@gmail.com>
2024-01-14 12:31:50 -0600

    progress

--- garland-django/graph/admin.py
+++ garland-django/graph/admin.py
@@ -1,7 +1,59 @@
+from django import forms
 from django.contrib import admin
+from django.forms.models import BaseInlineFormSet
+from .models import Node, NodeRelation
 
-from .models import Node, Person, Org
+class SubjectInline(admin.TabularInline):
+    model = NodeRelation
+    extra = 0
+    fk_name = "node_subject"
+    verbose_name = "Subject Relation"
 
-admin.site.register(Node)
-admin.site.register(Person)
-admin.site.register(Org)
+class DobjectInline(admin.TabularInline):
+    model = NodeRelation
+    extra = 0
+    fk_name = "node_dobject"
+    verbose_name = "Direct Object Relation"
+
+
+class CustomInlineFormSet(BaseInlineFormSet):
+        def get_queryset(self):
+          # Handle the case where the instance is new and unsaved
+          if not hasattr(self, '_queryset'):
+              if self.instance.pk is None:
+                  # Instance is new and unsaved, so it cannot have any relations yet
+                  self._queryset = self.model.objects.none()
+              else:
+                  # Get the standard queryset (forward relations)
+                  qs = super().get_queryset()
+
+                  # Get the reverse relations and combine them
+                  reverse_qs = self.model.objects.filter(node_dobject=self.instance)
+                  self._queryset = qs | reverse_qs
+
+          return self._queryset
+
+class NodeRelationInline(admin.TabularInline):
+    model = NodeRelation
+    formset = CustomInlineFormSet
+    extra = 0
+
+    # dummy foreign key field to satisfy Django System Check (we're overridding it anyways)
+    fk_name = 'node_subject'
+
+    def get_formset(self, request, obj=None, **kwargs):
+        formset = super().get_formset(request, obj, **kwargs)
+        formset.instance = obj
+        return formset
+    
+
+class NodeAdmin(admin.ModelAdmin):
+    inlines = [NodeRelationInline]
+
+# class NodeAdmin(admin.ModelAdmin):
+#     inlines = [
+#         SubjectInline,
+#         DobjectInline,
+#     ]
+
+admin.site.register(Node, NodeAdmin)
--- garland-django/graph/models.py
+++ garland-django/graph/models.py
@@ -5,24 +5,54 @@ from model_utils.managers import InheritanceManager
 class Node(models.Model):
     def __str__(self):
         return self.slug
-    
+
     objects = InheritanceManager()
+    nodes = models.ManyToManyField(
+        "self",
+        through="NodeRelation",
+        symmetrical=False,
+        through_fields=("node_subject", "node_dobject"),
+    )
 
     slug = models.SlugField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
 
-class Person(Node):
-    def __str__(self):
-        return f'{self.name_last}, {self.name_first}'
+class NodeRelation(models.Model):
+    kind_choices = {
+        "parent_of": "parent of",
+        "has_parent": "has parent",
+    }
 
-    name_last = models.CharField(max_length=200)
-    name_first = models.CharField(max_length=200)
+    def get_opposite_relationship(self):
+        opposites = {
+            "parent_of": "has_parent",
+            "has_parent": "parent_of",
+        }
+        return opposites.get(self.kind, "parent_of")
+
+    kind = models.CharField(max_length=200, choices=kind_choices)
+    order = models.IntegerField(default=0)
+
+    node_subject = models.ForeignKey(
+        Node, related_name="has_node", on_delete=models.CASCADE
+    )
+    node_dobject = models.ForeignKey(
+        Node, related_name="of_node", on_delete=models.CASCADE
+    )
 
 
-class Org(Node):
-    def __str__(self):
-        return self.name
+# class Person(Node):
+#     def __str__(self):
+#         return f"{self.name_last}, {self.name_first}"
 
-    name = models.CharField(max_length=200)
+#     name_last = models.CharField(max_length=200)
+#     name_first = models.CharField(max_length=200)
+
+
+# class Org(Node):
+#     def __str__(self):
+#         return self.name
+
+#     name = models.CharField(max_length=200)
